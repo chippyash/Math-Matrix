b2abce19a0171d2eb9c22e7e2926e428d76dc0fc
Ashley Kitson <ashley@zf4.biz>
2016-02-27 17:14:25 +0000

    add NotMarkovException

---
+++ src/chippyash/Math/Matrix/Exceptions/NotMarkovException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Markov
+ *
+ * @author Ashley Kitson
+ * @copyright Ashley Kitson, 2016, UK
+ * @license GPL V3+ See LICENSE.md
+ */
+
+namespace Chippyash\Math\Matrix\Exceptions;
+
+
+class NotMarkovException extends MathMatrixException
+{
+    protected $msgTpl = "Matrix is not suitable for a Markov Chain: ";
+
+    public function __construct($reason = '', $code = -1, $previous = null)
+    {
+        parent::__construct($this->msgTpl . $reason, $code, $previous);
+    }
+}
